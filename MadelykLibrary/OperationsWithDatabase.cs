a615bed0ca5ad3eb03c68b0a094e050c70697806
METEOR\andrii <sneyp67@gmail.com>
2016-12-16 23:04:29 +0200

    added add book with relationships

--- MadelykLibrary/OperationsWithDatabase.cs
+++ MadelykLibrary/OperationsWithDatabase.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MadelykLibrary.Model;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,35 @@ namespace MadelykLibrary
             {
                 a.Id = Guid.NewGuid();
                 db.Authors.Add(a);
+                db.SaveChanges();
+            }
+        }
+
+        public List<Category> GetAllCategory()
+        {
+            using (var db = new LibraryContext())
+            {
+               
+
+                return db.Categorys.ToList();
+            }
+        }
+        public List<Author> GetAllAuthors()
+        {
+            using (var db = new LibraryContext())
+            {
+                //var cat = new List<AuthorObs>();
+                var cat = db.Authors;
+                return cat.ToList();
+            }
+        }
+        public void AddBook(Book book)
+        {
+            using (var db = new LibraryContext())
+            {
+                book.Categoty = db.Categorys.ToList().Find(x => x.Id == book.Categoty.Id);
+                book.Author = db.Authors.ToList().Find(x => x.Id == book.Author.Id);
+                db.Books.Add(book);
                 db.SaveChanges();
             }
         }
--- MadelykLibrary/ViewModel/MainWindowRedactorViewModel.cs
+++ MadelykLibrary/ViewModel/MainWindowRedactorViewModel.cs
@@ -3,6 +3,7 @@ using GalaSoft.MvvmLight.CommandWpf;
 using MadelykLibrary.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,17 @@ namespace MadelykLibrary
     class MainWindowRedactorViewModel : ViewModelBase
     {
         private static OperationsWithDatabase connect;
+        public ObservableCollection<Category> Categories { get; set; }
+        public ObservableCollection<Author> Authors { get; set; }
+
+
         public MainWindowRedactorViewModel()
         {
             connect = new OperationsWithDatabase();
+            var SelectedCategory = new Category();
+            Categories = new ObservableCollection<Category>(connect.GetAllCategory());
+            Authors = new ObservableCollection<Author>(connect.GetAllAuthors());
+            var SelectedAuthor = new Author();
         }
         public string AddAuthorName { get; set; }
         public string AddAuthorSurname { get; set; }
@@ -41,7 +50,6 @@ namespace MadelykLibrary
             }
         }
         private string _addAuthorInfo;
-
         public string AddAuthorInfo
         {
             get { return _addAuthorInfo; }
@@ -58,7 +66,81 @@ namespace MadelykLibrary
 
 
 
+        public string AddBookName { get; set; }
 
+        public int countbook { get; set; }
+
+        private RelayCommand _addBook;
+
+        public RelayCommand AddBook
+        {
+            get
+            {
+                return _addBook ?? (_addBook = new RelayCommand(() =>
+                {
+                    AddBookInfo = "Added Book: " + AddBookName + " "+countbook;
+                    var book = new Book()
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = AddBookName,
+                        Count = countbook,
+                        Categoty = new Category(),
+                        Author = new Author()
+
+                    };
+                    book.Categoty.Id = SelectedCategory.Id;
+                    book.Author.Id = SelectedAuthor.Id;
+                    connect.AddBook(book);
+                    RaisePropertyChanged("AddBookInfo");
+                }, () => (AddBookName != null&& countbook>0&& SelectedCategory!=null&& SelectedAuthor!= null)));
+
+            }
+        }
+        private string _addBookInfo;
+
+        public string AddBookInfo
+        {
+            get { return _addBookInfo; }
+            set
+            {
+                if (_addBookInfo == value) return;
+                _addBookInfo = value;
+
+                RaisePropertyChanged("AddBookInfo");
+
+            }
+        }
+
+
+        private Category _selectedCategory;
+
+        public Category SelectedCategory
+        {
+            get { return _selectedCategory; }
+
+            set
+            {
+                if (_selectedCategory == value) return;
+                _selectedCategory = value;
+                RaisePropertyChanged("SelectedCategory");
+
+            }
+        }
+
+        private Author _selectedAuthor;
+
+        public Author SelectedAuthor
+        {
+            get { return _selectedAuthor; }
+
+            set
+            {
+                if (_selectedAuthor == value) return;
+                _selectedAuthor = value;
+                RaisePropertyChanged("SelectedAuthor");
+
+            }
+        }
 
 
 
